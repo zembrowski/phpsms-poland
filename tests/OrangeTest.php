9cfc052aa78446e02900df6edd0a32761a2a458d
Krzysztof Tomasz Zembrowski <http://krzysztof.zembrowski.com>
2016-02-09 00:11:43 +0100

    After test 4
    ReflectionMethod reference

--- tests/OrangeTest.php
+++ tests/OrangeTest.php
@@ -25,7 +25,7 @@ class OrangeTest extends \PHPUnit_Framework_TestCase
      */
     public function testFree()
     {
-        $method = new ReflectionMethod('zembrowski\SMS\Orange', 'free');
+        $method = new \ReflectionMethod('zembrowski\SMS\Orange', 'free');
 
         $method->setAccessible(TRUE);
 
