1ca0f9e575fea3d58ae52b8abe8a8e0f7bd2979d
Krzysztof Tomasz Zembrowski <http://krzysztof.zembrowski.com>
2017-05-17 00:58:50 +0200

    Travis CI with Coverage test

--- scripts/phpunit-with-coveralls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-mkdir -p build/logs
-vendor/bin/phpunit --coverage-clover=build/logs/clover.xml
-composer require satooshi/php-coveralls
-php vendor/bin/coveralls build/logs/clover.xml -v
